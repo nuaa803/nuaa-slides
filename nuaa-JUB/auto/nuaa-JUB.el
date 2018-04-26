596356f7d17d2e91e8f35c70d185c722304f2a92
Felix Ding <uraplutonium@yandex.com>
2018-04-26 19:03:35 +0800

    nuaa-JUB and nuaa-AMS updated.

--- nuaa-JUB/auto/nuaa-JUB.el
+++ nuaa-JUB/auto/nuaa-JUB.el
@@ -6,12 +6,12 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("helvet" "scaled")))
    (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -28,6 +28,8 @@
     "Fig:latex_beamer"
     "Sec:model"
     "Sec:experiments")
+   (LaTeX-add-environments
+    '("options" LaTeX-env-args ["argument"] 0))
    (LaTeX-add-bibliographies
     "bibliography_file"))
  :latex)
